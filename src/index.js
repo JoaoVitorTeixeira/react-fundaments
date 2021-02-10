842e48324959768befd148e3685080d75283f8c3
João Vitor Teixeira <joaovitorteixeira10.jvt@gmail.com>
2021-02-10 11:55:31 -0300

    feat: :sparkles: add frgment component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 
 import First from './components/basics/First'
+import WithParameter from './components/basics/WithParameter.tsx'
+import Fragment from './components/basics/Fragment.tsx'
 
 ReactDOM.render(
     <div>
         <First></First>
+        <WithParameter></WithParameter>
+        <Fragment></Fragment>
     </div>,
     document.getElementById('root'))
